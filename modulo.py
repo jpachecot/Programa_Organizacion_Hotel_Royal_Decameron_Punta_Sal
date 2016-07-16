ac773b3bcf3a57f1c505019e069c484132353786
alfredo223 <marce_cv_13@hotmail.com>
2016-07-16 16:35:36 -0500

    Update modulo.py

--- modulo.py
+++ modulo.py
@@ -1,1 +1,8 @@
-
+def menu():
+  print("1---->Inicio.")
+  print("2---->Reservaciones.")
+  print("3---->Eventos.")
+  print("4---->Tours.")
+  print("5---->Promociones.")
+  print("6---->Descripción de Habitaciones.")
+  print("7---->Descripción de Servicios e Instalaciones.")
